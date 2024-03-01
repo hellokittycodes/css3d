40187ae555ef28b3e9f46376ba5757c9b5769184
allergic2everything <ritika.myana@gmail.com>
2024-03-01 10:47:40 -0500

    main

--- cube.js
+++ cube.js
@@ -1,18 +1,15 @@
 function rotateCube(e) {
-    var cube = document.getElementsByClassName("box"); // Change "cube" to "box"
+    var cube = document.getElementById("cube"); // Change getElementsByClassName to getElementById
     var x = e.clientX - window.innerWidth / 2;
-    var y = e.clientY - window.innerHeight /2;
+    var y = e.clientY - window.innerHeight / 2;
     var q = 0.15;
-    var i;
- 
+
     x = x * q * 1.25;
     y = -y * q * 1.25;
- 
-    for(i=0; i < cube.length; i++)
-    {
-        cube[i].style.transform = "rotateY(" + x + "deg) rotateX(" + y + "deg)";
-    }
- }
- 
- document.addEventListener("mousemove", rotateCube);
+
+    cube.style.transform = "rotateY(" + x + "deg) rotateX(" + y + "deg)";
+}
+
+document.addEventListener("mousemove", rotateCube);
+
  
