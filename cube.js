543e74b6b3fdfbd82a5b331bcedce049f07edbf9
allergic2everything <ritika.myana@gmail.com>
2024-03-04 10:26:45 -0500

    attempt 3

--- cube.js
+++ cube.js
@@ -1,4 +1,5 @@
-$(document).mousemove(function(e)
+//commented that out let's see if attempt two works 
+/* $(document).mousemove(function(e)
 {
     var cube = document.getElementById("cube");
     $( "#box" ).on( "mousemove", function() 
@@ -15,4 +16,19 @@ $(document).mousemove(function(e)
         }
         });
     });
-});
+}); */ 
+
+// ATTEMPT TWO 
+jQuery.fn.rotate = function(degrees) {
+    $(this).css({'transform' : 'rotate('+ degrees +'deg)'});
+    return $(this);
+};
+
+$('.box').mousemove(function(e)
+{
+    $('.rotate;').animate
+    ({ 
+        //idk what to do here 
+    })
+
+}); 
